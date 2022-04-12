960ef5dcf394ab384c73b4d186056319b5781f8e
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2022-04-12 16:50:51 -0400

    ver bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 # from scriptforge_stubs import __version__
 PKG_NAME = 'types-sourceforge'
-VERSION = "0.1.5"
+VERSION = "0.1.7"
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
