565474a45f7fecedcb571e35530fd1fb799b6e66
Paul Moss <vibrationoflife@protonmail.com>
2022-04-25 18:27:38 -0400

    ver bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 # from scriptforge_stubs import __version__
 PKG_NAME = 'types-scriptforge'
-VERSION = "0.1.3"
+VERSION = "1.0.0"
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
